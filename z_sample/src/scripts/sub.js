7ada4e84896b84d706fa9008360957edf407a30c
ginmaru <56679565+ginmaruGH@users.noreply.github.com>
2021-05-11 15:11:41 +0900

    update

--- z_sample/src/scripts/sub.js
+++
@@ -1,8 +0,0 @@
-export const hello = () => {
-  console.log("JavaScript import成功");
-}
-
-export const example = () => {
-  `Sensible webpack 5 boilerplate using Babel and PostCSS with a hot dev server
-  and an optimized production build.`
-}
